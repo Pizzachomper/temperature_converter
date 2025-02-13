8925c86899dc5f2b136143d8469ceb69b43e7f32
Pizzachomper <100880007+Pizzachomper@users.noreply.github.com>
2025-02-14 11:03:45 +1300

    Video 6, Timestamp: 2:49

---
+++ Conversion_rounding.py
@@ -0,0 +1,36 @@
+def round_ans(val):
+    """
+    Rounds temperature to nearest degree
+    Number needs to be rounded
+    Number rounded to nearest degree
+    """
+    var_rounded = (val * 2 + 1) // 2
+    return f"{var_rounded:.0f}"
+
+def to_celsius(to_convert):
+    """
+    Converts from F to C
+    """
+    answer = (to_convert - 32) * 5 / 9
+    return round_ans(answer)
+
+
+def to_fahrenheit(to_convert):
+    """
+    Converts from C to F
+    """
+
+# Main routine / testing here
+to_c_test = [0, 100, -459]
+to_f_test = [0, 100, 40, -273]
+
+for item in to_f_test:
+    ans = to_fahrenheit(item)
+    print(f"{item}C is {ans}F")
+
+print()
+
+for item in to_c_test:
+    ans = to_celsius(item)
+    print(f"{item}F is {ans}C")
+
