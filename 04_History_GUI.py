f9c3bd1b96c4fde1f305d08c9e653d0b04907643
Pizzachomper <100880007+Pizzachomper@users.noreply.github.com>
2025-02-21 10:05:27 +1300

    video 10, 2:08

--- 04_History_GUI.py
+++ 04_History_GUI.py
@@ -35,7 +35,51 @@ class HistoryExport:
     """
 
     def __init__(self, partner):
-        pass
+        # setup dialouge box and background colour
+
+        green_back = "#D5E8D4"
+        peach_back = "#ffe6cc"
+        
+        self.history_box = Toplevel()
+
+        # disable history button
+        partner.to_history_button.config(state=DISABLED)
+
+        # If users press cross at top, closes history and releases history button
+        self.history_box.protocol('WH_DELETE_WINDOW',
+                                  partial(self.close_history, partner))
+
+        self.history_frame = Frame(self.history_box)
+        self.history_frame.grid()
+
+        # strings for long tasks 
+        recent_intro_txt = ("Below are your recent calculations - showing "
+                            "3/3 calculations. All calculations are shown to the nearest degree")
+
+        export_instructions_txt = ("Please push <export> to save your calculations in file."
+                                   "If the filename already exsists, it will be replaced.")
+
+        calculations = ""
+
+        # Label list (label text | format | bg)
+        history_labels_list = [
+            ["History / Export", ("Arial", "16", "bold"), None],
+            [recent_intro_txt, ("Arial", "11"), None],
+            ["calculation list", ("Arial", "14"), green_back],
+            [export_instructions_txt, ("Arial", "11"), None]
+        ]
+
+        history_label_ref = []
+        for count, item in enumerate(history_labels_list):
+            make_label = Label(self.history_box, text=item[0], font=item[1], 
+                               bg=item[2],
+                               wraplength=300, justify="left", pady=10, padx=20)
+            make_label.grid(row=count)
+
+            history_label_ref.append(make_label)
+
+        # Retrieve export instruction label so that we can configure it to show the filename if the user exports to file
+        self.ex
 
     def close_history(self, partner):
         pass
